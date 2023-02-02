35fe944e96b65d9a065179fb651a61e84f7ffd43
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-02-02 21:30:13 +0000

    change secret key settings

--- tasks_management/settings.py
+++ tasks_management/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-8c3g&4)zys2rqg2v+u0%g$fc5^@x=bw8n_jew84tgy6vd(iwc='
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
