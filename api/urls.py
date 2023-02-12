6f59fcc6acdba95755a0517c9b516de04f0994ed
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-02-12 01:09:33 +0000

    added a post view

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, getTasks, getSubTasks
+from .views import index, getTasks, getSubTasks, addTask
 
 # ============================================================================#
 urlpatterns = [
     path('', index, name='api_index'),
     path('tasks/', getTasks, name='tasks'),
+    path('add_task/', addTask, name='add_task'),
     path('sub_tasks/', getSubTasks, name='sub_tasks'),
 ]
--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 from base.models import Task, Sub_Task
 from . serializers import TaskSerializer, SubTaskSerializer
@@ -10,8 +11,11 @@ from . serializers import TaskSerializer, SubTaskSerializer
 # ============================================================================#
 
 
+@api_view(['GET'])
 def index(request):
-    return HttpResponse('<h1>API application</h1>')
+    api_endpoints = {'Description': 'a list of end points for tasks api', 'index': 'api/v1/',
+                     'tasks': 'api/v1/tasks/', 'subtask': 'api/v1/sub_tasks/'}
+    return Response(api_endpoints)
 
 
 @api_view(['GET'])
@@ -26,3 +30,12 @@ def getSubTasks(request):
     sub_tasks = Sub_Task.objects.all()
     serializer = SubTaskSerializer(sub_tasks, many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def addTask(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=HTTP_201_CREATED)
+    return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
