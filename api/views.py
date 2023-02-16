22d940bf8e55b3fcb9f01a00928c4d26cb96f39f
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-02-16 22:29:41 +0000

    added permission classes

--- api/views.py
+++ api/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 from base.models import Task, Sub_Task
 from . serializers import TaskSerializer, SubTaskSerializer
-
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.views import APIView
 # ============================================================================#
 
 
@@ -33,6 +35,8 @@ def getSubTasks(request):
 
 
 @api_view(['POST'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAdminUser])
 def addTask(request):
     serializer = TaskSerializer(data=request.data)
     if serializer.is_valid():
